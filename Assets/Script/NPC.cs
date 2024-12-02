fbcb9e118a89ea005b66dc24c37b34b2076f08c2
VictorArt97 <Bakalovvictor@gmail.com>
2024-12-02 18:20:05 +0100

    Crear npc

---
+++ Assets/Script/NPC.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+   
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
